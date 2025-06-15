12f5541f149b389a700d6579ffee3986a67ed4ee
Resul KORKMAZ <resulkorkmaz@live.com>
2025-06-15 18:11:52 +0300

    fix: Simplify ESLint configuration and remove project parser options

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -2,9 +2,5 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@onlineusta/eslint-config/react-internal.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-}; 
+  extends: ["next/core-web-vitals"],
+};
